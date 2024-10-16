9c0552325ca79aa1d7faf0f11206eee11e9f9dc1
Mike <watsonmichael919@gmail.com>
2024-10-16 04:41:27 -0500

    cleaned up code, removed unused and redundant stuff

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::VecDeque;
 struct Stack<T> {
     stack: Vec<T>,
 }
@@ -55,12 +54,8 @@ pub fn infix_to_postfix(input: &str) {
             } else {
                 loop {
                   if let Some(value) = next_op {
-                    //   println!("=========");
-                    //   println!("{value}");
-                    //   println!("=========");
                       let entry_precedence = precedence(*entry);
                       let stack_top_precendence = precedence(*value);
-                    //   println!("{stack_top_precendence}");
                       
                       if stack_top_precendence >= entry_precedence {
                         let popped_item = op_stack.pop().unwrap();
@@ -76,19 +71,6 @@ pub fn infix_to_postfix(input: &str) {
                       break
                   }
                 }
-            //   let op = *next_op.unwrap();
-            //   let entry_precedence = precedence(*entry);
-            //   let stack_top_precendence = precedence(op);
-
-            //   if stack_top_precendence >= entry_precedence {
-            //     let popped_item = op_stack.pop().unwrap();
-            //     println!("{popped_item} ");
-                // break;
-            //   } 
-            //   else {
-            //     op_stack.push(*entry);
-            //     // break;
-            //   }
             }
           },
           ')' => {
@@ -116,64 +98,7 @@ pub fn infix_to_postfix(input: &str) {
   }
 }
 
-pub fn infix_to_postfix2(input: &str) {
-  let char_array: Vec<char> = input.chars().collect();
-  let mut operator_stack: Stack<char> = Stack::new();
-
-  // FIFO
-  // use push_back to add
-  // pop_front to remove
-  let mut output_stack: VecDeque<char> = VecDeque::new();
-  // let mut output_stack: Stack<char> = Stack::new();
-
-  // precedence:
-  //  * and / have higher than + and - 
-  //  * has the same as / and + the same as - 
-  for entry in &char_array {
-      match entry {
-          '+' | '-' | '/' | '*' => {
-            if operator_stack.is_empty() {
-              operator_stack.push(*entry);
-            } else {
-              let next_operator = operator_stack.peek();
-              let operator = *next_operator.unwrap();
-              let entry_precedence = precedence(*entry);
-              let stack_top_precendence = precedence(operator);
-              if entry_precedence > stack_top_precendence {
-                operator_stack.push(*entry);
-
-              }
-
-              if entry_precedence <= stack_top_precendence  {
-                // let popped_item = operator_stack.pop().unwrap();
-                // println!("{popped_item} ");
-                loop {
-                  if let Some(value) = operator_stack.pop() {
-  
-                    
-                  }
-                }
-              }
-
-            }
-          },
-          ')' => {
-
-          },
-          '(' => {
-
-          },
-          _ => {
-              output_stack.push_back(*entry);
-          }
-      }
-  }
-
-}
-
 fn main() {
-    // infix_to_postfix("1+2*3-4");
-
     println!("======================");
     infix_to_postfix("1+2*(3-4)*(5+6*7)-8");
     println!("======================");
